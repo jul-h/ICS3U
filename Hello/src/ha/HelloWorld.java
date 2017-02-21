2c9fb5ef2c333516fa9c68f904531b9680689f1a
Julie Ha <julieha2013@gmail.com>
2017-02-21 11:24:18 -0500

    This is a program that says "Hello, World!" in the console.

---
+++ Hello/src/ha/HelloWorld.java
@@ -0,0 +1,17 @@
+package ha;
+/**
+ * 
+ * HelloWorld.java
+ * This program prints out "Hello, World!" in the console.
+ * February 21, 2017
+ * @author Julie Ha
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+
+	}
+
+}

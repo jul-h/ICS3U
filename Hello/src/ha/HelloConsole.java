93e614c5a3d08c87767f0edd1683efbc7fe93e6a
Julie Ha <julieha2013@gmail.com>
2017-02-27 11:09:16 -0500

    This program is a decision-based game

---
+++ Hello/src/ha/HelloConsole.java
@@ -0,0 +1,12 @@
+package ha;
+
+import hsa_new.Console;
+
+public class HelloConsole {
+
+	public static void main(String[] args) {
+		Console c = new Console();
+		c.println("Hello");
+	}
+
+}
---
+++ Stranger Game/src/ha/StrangerGame.java
@@ -0,0 +1,18 @@
+package ha;
+/**
+ * 
+ * StrangerGame.java
+ * This program is a decision-based game.
+ * February 27, 2017
+ * @author Julie Ha
+ * @author Lachlan Gray
+ *
+ */
+public class StrangerGame {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}

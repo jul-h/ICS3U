23901817d17f799e1635ddcfc1b36da7baf0d4be
Julie Ha <julieha2013@gmail.com>
2017-03-19 02:56:23 -0400

    Added more images and some audio

--- Stranger Game/src/ha/StrangerGame.java
+++ Stranger Game/src/ha/StrangerGame.java
@@ -4,10 +4,11 @@ import java.awt.Color;
 
 import hsa_new.Console;
 
+import javax.xml.crypto.Data;
+
 import java.io.File;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.xml.crypto.Data;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -17,32 +18,45 @@ import javax.imageio.ImageIO;
 
 /**
  *
- * StrangerGame.java This program is a decision-based game. 
+ * StrangerGame.java 
+ * This program is a decision-based game. 
  * Created February 27, 2017
- * @author Julie Ha
- * @author Lachlan Gray
+ * @author Julie Ha & Lachlan Gray
  *
  */
 public class StrangerGame { 
-	static Console c = new Console(30,100);
+	static Console c = new Console(39, 95, 13, "S T R A N G E R"); // The output console
 	static String input;
 	
 	// print each letter one by one
+	/*
 	public static void text(String data) throws InterruptedException{
 		for (int i = 0; i < data.length(); i++) {
-			//c.setTextBackgroundColor(Color.);
+			Color transparent = new Color(0, 0, 0, 50);
+			c.setTextBackgroundColor(transparent);
 			c.print(data.charAt(i));
-			Thread.sleep(50);
-			/*
-			String sentenceEnd = ". ";
-			if (data.contains(sentenceEnd)){
+			if (data.contains(". ")){
 				Thread.sleep(1000);
-			} else {
-				Thread.sleep(60);
+			}
+			Thread.sleep(50);
+		}
+	} // text method
+	*/
+	
+	public static void text(String data) throws InterruptedException{
+		char[] stringToCharArray = data.toCharArray();
+		for (int i = 0; i < stringToCharArray.length; i++){
+			Color transparent = new Color(0, 0, 0, 50);
+			//c.setTextBackgroundColor(transparent);
+			c.print(stringToCharArray[i]);
+			/*
+			if (data.contains(". ")){
+				Thread.sleep(1000);
 			}
 			*/
+			//Thread.sleep(50);
 		}
-	}
+	} // text method
 
 	// check if input is valid
 	public static void checkInput() throws InterruptedException{
@@ -52,36 +66,46 @@ public class StrangerGame {
 			c.setTextColor(Color.black);
 			input = c.readLine();
 		}
-	}
+	} //checkInput method
 
 	// main method
 	public static void main(String[] args) throws InterruptedException {
 		Boolean flag = false; // used in int checks
-	
+		
+		//load images
 		BufferedImage jpgStreet = null;
-		BufferedImage jpgDog = null;
+		BufferedImage jpgStranger = null;
+		BufferedImage jpgStrangerKnife = null;
+		BufferedImage jpgFriends = null;
+		BufferedImage pngMan = null;
 		BufferedImage pngScrewdriver = null;
 		BufferedImage png2x4 = null;
 		BufferedImage pngBaseballBat = null;
-		// String a; //variable to see if the user pressed the space bar
-		// continue with the game
+		BufferedImage jpgDoor = null;
+		BufferedImage jpgDog = null;
+		BufferedImage jpgCoats = null;
+		BufferedImage jpgHostage = null;
+		
 		do { 
 			c.clear();
-			 try{ 
-				 jpgStreet = ImageIO.read(new File("resources/images/snowyStreet.jpg")); 
-			 } catch (IOException e) {
-			 System.err.println("There was an error loading the image.");
-			 e.printStackTrace(); 
-			 } 
-			 c.drawImage(jpgStreet, -100, -50, 1067, 711, null);
+			
+			try{ 
+				jpgStreet = ImageIO.read(new File("resources/images/snowyStreet.jpg")); 
+			} catch (IOException e) {
+				System.err.println("There was an error loading the image.");
+				e.printStackTrace(); 
+			} 
+			c.drawImage(jpgStreet, -100, 100, 941, 628, null);
+			c.setTextColor(Color.black);
 			text("It's a cold winter night... "
-					+ "You and your friend were just on your way home from a movie, when suddenly, you realize that you guys need bus fare. What do you do?" 
+					+ "You and your friend were just on your way home from a movie, when "
+					+ '\n'+ "suddenly, you realize that you guys need bus fare. What do you do?" 
 					+ '\n');
-			c.setTextColor(new Color(80, 145, 180));
+			c.setTextColor(new Color(0, 204, 204));
 			text("a)");
 			c.setTextColor(Color.black);
 			text(" Find an ATM." + '\n');
-			c.setTextColor(new Color(80, 145, 180));
+			c.setTextColor(new Color(0, 204, 204));
 			text("b)");
 			c.setTextColor(Color.black);
 			text(" Ask the mysterious man nearby for bus fare." + '\n');
@@ -92,29 +116,52 @@ public class StrangerGame {
 			checkInput();
 			if (input.equalsIgnoreCase("a")) {
 				c.clear();
+				try{ 
+					jpgStranger = ImageIO.read(new File("resources/images/stranger.jpg")); 
+				} catch (IOException e) {
+					System.err.println("There was an error loading the image.");
+					e.printStackTrace(); 
+				} 
+				c.drawImage(jpgStranger, 0, 100, 782, 722, null);
+				//insert footsteps audio
 				text("The mysterious man follows you. What do you do?" + '\n');
-				c.setTextColor(new Color(80, 145, 180)); // change text color to blue
+				c.setTextColor(new Color(0, 204, 204)); // change text color to blue
 				text("a)");
 				c.setTextColor(Color.black);
 				text(" Ignore him." + '\n');
-				c.setTextColor(new Color(80, 145, 180));
+				c.setTextColor(new Color(0, 204, 204));
 				text("b)");
 				c.setTextColor(Color.black); 
 				text(" Address him." + '\n');
 				input = c.readLine();
-				// prevents program from crashing if user enters invalid response
-				checkInput();
+				checkInput(); // prevents program from crashing if user enters invalid response
 				if (input.equalsIgnoreCase("a")) {
 					c.clear();
-					text("You and your friend get killed by the man." + '\n');
+					try{ 
+						jpgStrangerKnife = ImageIO.read(new File("resources/images/strangerKnife.jpg")); 
+					} catch (IOException e) {
+						System.err.println("There was an error loading the image.");
+						e.printStackTrace(); 
+					} 
+					c.drawImage(jpgStrangerKnife, 0, 100, 782, 722, null);
+					//insert knife stabbing sounds, screaming
+					text("You and your friend get killed by the man." + '\n'); //ending
 					Thread.sleep(1000);
 				} else {
+					c.clear();
+					try{ 
+						pngMan = ImageIO.read(new File("resources/images/man.png")); 
+					} catch (IOException e) {
+						System.err.println("There was an error loading the image.");
+						e.printStackTrace(); 
+					} 
+					c.drawImage(pngMan, 0, 100, 782, 722, null);
 					text("He invites you both to his apartment. Do you accept his invitation?" + '\n');
-					c.setTextColor(new Color(80, 145, 180));
+					c.setTextColor(new Color(0, 204, 204));
 					text("a)");
 					c.setTextColor(Color.black);
 					text(" Yes" + '\n');
-					c.setTextColor(new Color(80, 145, 180));
+					c.setTextColor(new Color(0, 204, 204));
 					text("b)");
 					c.setTextColor(Color.black);
 					text(" No" + '\n');
@@ -122,15 +169,23 @@ public class StrangerGame {
 					checkInput();
 					if (input.equalsIgnoreCase("a")) {
 						c.clear();
-						text("You and your friend go to his apartment and have an awesome time!" + '\n');
+						try{ 
+							jpgFriends = ImageIO.read(new File("resources/images/friends.jpg")); 
+						} catch (IOException e) {
+							System.err.println("There was an error loading the image.");
+							e.printStackTrace(); 
+						} 
+						c.drawImage(jpgFriends, 0, 100, 782, 722, null);
+						text("You and your friend go to his apartment and have an awesome time!" + '\n'); //ending
 						Thread.sleep(1000);
 					} else {
+						c.clear();
 						text("He robs both of you and leaves you on the street. What do you do?" + '\n');
-						c.setTextColor(new Color(80, 145, 180));
+						c.setTextColor(new Color(0, 204, 204));
 						text("a)");
 						c.setTextColor(Color.black);
 						text(" Follow him." + '\n');
-						c.setTextColor(new Color(80, 145, 180));
+						c.setTextColor(new Color(0, 204, 204));
 						text("b)");
 						c.setTextColor(Color.black);
 						text(" Find help." + '\n');
@@ -138,12 +193,19 @@ public class StrangerGame {
 						checkInput();
 						if (input.equalsIgnoreCase("a")) {
 							c.clear();
+							try{ 
+								jpgDoor = ImageIO.read(new File("resources/images/door.jpg")); 
+							} catch (IOException e) {
+								System.err.println("There was an error loading the image.");
+								e.printStackTrace(); 
+							} 
+							c.drawImage(jpgDoor, 150, 75, 429, 644, null);
 							text("You end up at his apartment. The door is unlocked. Do you enter?" + '\n');
-							c.setTextColor(new Color(80, 145, 180));
+							c.setTextColor(new Color(0, 204, 204));
 							text("a)");
 							c.setTextColor(Color.black);
 							text(" Yes." + '\n');
-							c.setTextColor(new Color(80, 145, 180));
+							c.setTextColor(new Color(0, 204, 204));
 							text("b)");
 							c.setTextColor(Color.black);
 							text(" No." +'\n');
@@ -152,30 +214,52 @@ public class StrangerGame {
 							if (input.equalsIgnoreCase("a")) {
 								int x = (int) (Math.random() * 2 + 1);
 								if (x == 2) {
+									c.clear();
 									try{
 										jpgDog = ImageIO.read(new File("resources/images/dog.jpg"));
 									} catch (IOException e) {
 										System.err.println("There was an error loading the image.");
 										e.printStackTrace();
 									}
-									c.drawImage(jpgDog, 0, 0, null);
-									text("You and your friend are devoured by the biggest rottweiler you've ever seen." + '\n');
-									Thread.sleep(1000);
+									c.drawImage(jpgDog, -105, 95, null);
+									try {
+										Clip dog = AudioSystem.getClip();
+										dog.open(AudioSystem.getAudioInputStream(new File("resources/audio/dog.wav")));
+										dog.start();
+										text("You and your friend are devoured by the biggest rottweiler you've ever seen." + '\n'); //ending
+										Thread.sleep(5000);
+										dog.stop();
+									} catch (Exception e) {
+										e.printStackTrace();
+									}
 								} else {
-									text("You find your stuff and get away safely." + '\n');
+									c.clear();
+									try{ 
+										jpgCoats = ImageIO.read(new File("resources/images/coats.jpg")); 
+									} catch (IOException e) {
+										System.err.println("There was an error loading the image.");
+										e.printStackTrace(); 
+									} 
+									c.drawImage(jpgCoats, 170, 75, 429, 644, null);
+									text("You find your stuff and get away safely." + '\n'); //ending
 									Thread.sleep(1000);
 								}
 							} else {
+								//insert sad people silhouette
 								text("You give up and go home defeated." + '\n');
 								Thread.sleep(1000);
 							}
 						} else {
 							int x = (int) (Math.random() * 2 + 1);
 							if (x == 1) {
-								text("The man is apprehended, and you get your stuff back." + '\n');
+								//insert man in handcuffs
+								//insert handcuffs sound??
+								text("The man is apprehended, and you get your stuff back." + '\n'); //ending
 								Thread.sleep(1000);
 							} else {
-								text("You wake up in the hospital" + '\n');
+								//insert hospital pic
+								//insert hospital heart monitor beeping sound thing??
+								text("You wake up in the hospital" + '\n'); //ending
 								Thread.sleep(1000);
 							}
 						}
@@ -184,15 +268,18 @@ public class StrangerGame {
 			} else {
 				int x = (int) (Math.random() * 3);
 				if (x == 1) {
-					text("You and your friend get your bus fare and go home." + '\n');
+					//insert two people on snowy street
+					text("You and your friend get your bus fare and go home." + '\n'); //ending
 				} else {
 					c.clear();
+					//insert man with knife in snowy street
+					//insert knife sound
 					text("The man pulls a knife on you and your friend. What do you do?" + '\n');
-					c.setTextColor(new Color(80, 145, 180));
+					c.setTextColor(new Color(0, 204, 204));
 					text("a)");
 					c.setTextColor(Color.black);
 					text(" Fight." + '\n');
-					c.setTextColor(new Color(80, 145, 180));
+					c.setTextColor(new Color(0, 204, 204));
 					text("b)");
 					c.setTextColor(Color.black);
 					text(" Run." + '\n');
@@ -200,15 +287,24 @@ public class StrangerGame {
 					checkInput();
 					if (input.equalsIgnoreCase("a")) {
 						c.clear();
-						text("You die tragically, but your friend escapes safely" + '\n'); // ending
+						//insert strangerKnife with running man in background
+						//insert running sound, knife stabbing, screaming
+						text("You die tragically, but your friend escapes safely." + '\n'); // ending
 					} else {
 						c.clear();
-						text("You get away but the man apprehends your friend. What do you do?" + '\n');
-						c.setTextColor(new Color(80, 145, 180));
+						try{
+							jpgHostage = ImageIO.read(new File("resources/images/hostage.jpg"));
+						} catch (IOException e) {
+							System.err.println("There was an error loading the image.");
+							e.printStackTrace();
+						}
+						c.drawImage(jpgHostage, 160, 55, 429, 644, null);
+						text("You get away but the man holds your friend hostage. What do you do?" + '\n');
+						c.setTextColor(new Color(0, 204, 204));
 						text("a)");
 						c.setTextColor(Color.black);
 						text(" Run." + '\n');
-						c.setTextColor(new Color(80, 145, 180));
+						c.setTextColor(new Color(0, 204, 204));
 						text("b)");
 						c.setTextColor(Color.black);
 						text(" Help your friend." + '\n');
@@ -216,25 +312,27 @@ public class StrangerGame {
 						checkInput();
 						if (input.equalsIgnoreCase("a")) {
 							c.clear();
+							//insert strangerKnife with running man in background
+							//insert running sound, knife stabbing, screaming
 							text("You escape safely but your friend is slain." + '\n'); // ending
-
 						} else {
 							text("Do you try to negotiate or fight?" + '\n');
-							c.setTextColor(new Color(80, 145, 180));
+							c.setTextColor(new Color(0, 204, 204));
 							text("a)");
 							c.setTextColor(Color.black);
 							text(" Negotiate." + '\n');
-							c.setTextColor(new Color(80, 145, 180));
+							c.setTextColor(new Color(0, 204, 204));
 							text("b)");
 							c.setTextColor(Color.black);
 							text(" Fight." + '\n');
 							input = c.readLine();
 							checkInput();
 							if (input.equalsIgnoreCase("a")) {
+								//insert man in alleyway pic
 								x = (int) (Math.random() * 11);
 								System.err.println(x); // for testing purposes
 								text("Guess a number from ");
-								c.setTextColor(new Color(80, 145, 180));
+								c.setTextColor(new Color(0, 204, 204));
 								text("1 to 10");
 								c.setTextColor(Color.black);
 								text(", you have 3 chances to guess correctly." + '\n');
@@ -259,6 +357,7 @@ public class StrangerGame {
 										} // try/catch end
 									} // while structure end
 									if (guess == x) {
+										//insert two people leaving alleyway/snowy street
 										i = 4;
 										c.clear();
 										text("You negotiated successfully, you and your friend go home safely" + '\n'); // ending
@@ -266,17 +365,25 @@ public class StrangerGame {
 										text("You guessed incorrectly. Try again." + '\n');
 										if (i == 2){
 											c.clear();
+											try{ 
+												jpgStrangerKnife = ImageIO.read(new File("resources/images/strangerKnife.jpg")); 
+											} catch (IOException e) {
+												System.err.println("There was an error loading the image.");
+												e.printStackTrace(); 
+											} 
+											c.drawImage(jpgStrangerKnife, 0, 100, 782, 722, null);
+											//insert knife stabbing sound, screaming
 											text("You failed to negotiate and the man kills both of you." + '\n'); // ending
 										}
 									}
 								} // for loop end
 							} else {
 								text("Do you fight immediately or find a weapon?" + '\n');
-								c.setTextColor(new Color(80, 145, 180));
+								c.setTextColor(new Color(0, 204, 204));
 								text("a)");
 								c.setTextColor(Color.black);
 								text(" Fight immediately." + '\n');
-								c.setTextColor(new Color(80, 145, 180));
+								c.setTextColor(new Color(0, 204, 204));
 								text("b)");
 								c.setTextColor(Color.black);
 								text(" Find a weapon." + '\n');
@@ -284,6 +391,14 @@ public class StrangerGame {
 								checkInput();
 								if (input.equalsIgnoreCase("a")) {
 									c.clear();
+									try{ 
+										jpgStrangerKnife = ImageIO.read(new File("resources/images/strangerKnife.jpg")); 
+									} catch (IOException e) {
+										System.err.println("There was an error loading the image.");
+										e.printStackTrace(); 
+									} 
+									c.drawImage(jpgStrangerKnife, 0, 100, 782, 722, null);
+									//insert running sound, knife stabbing, screaming
 									text("You and your friend die because you attacked an armed man without a weapon." +'\n'); // ending
 								} else {
 									c.clear();
@@ -323,11 +438,11 @@ public class StrangerGame {
 										text(weapons[i]);
 										c.setTextColor(Color.black);
 										text(". Do you pick it up or find a better weapon?" + '\n');
-										c.setTextColor(new Color(80, 145, 180));
+										c.setTextColor(new Color(0, 204, 204));
 										text("a)");
 										c.setTextColor(Color.black);
 										text(" Pick it up." + '\n');
-										c.setTextColor(new Color(80, 145, 180));
+										c.setTextColor(new Color(0, 204, 204));
 										text("b)");
 										c.setTextColor(Color.black);
 										text(" Find a better weapon." + '\n');
@@ -338,15 +453,26 @@ public class StrangerGame {
 											int weaponStrength = (i + 1) * 2; // determines likelihood of success
 											x = (int) (Math.random() * 9);
 											if (x <= weaponStrength) {
+												//insert two people walking out of alleyway
 												text("You overpower the man, you and your friend escape safely." +'\n'); // ending
 												i = 3;
 											} else {
+												try{ 
+													jpgStrangerKnife = ImageIO.read(new File("resources/images/strangerKnife.jpg")); 
+												} catch (IOException e) {
+													System.err.println("There was an error loading the image.");
+													e.printStackTrace(); 
+												} 
+												c.drawImage(jpgStrangerKnife, 0, 100, 782, 722, null);
+												//insert knife stabbing sound, screaming
 												text("You fail your attack, and you both die." + '\n'); // ending
 												i = 3;
 											}
 
 										} else {
 											c.clear();
+											//insert strangerKnife with man in background with a baseball bat 
+											//insert knife stabbing sound, screaming
 											text("Your friend died because you took too long to decide on a weapon to use." + '\n');
 										}
 									} // for end
@@ -363,11 +489,11 @@ public class StrangerGame {
 
 			}
 			text("Play again?" + '\n');
-			c.setTextColor(new Color(80, 145, 180));
+			c.setTextColor(new Color(0, 204, 204));
 			text("a)");
 			c.setTextColor(Color.black);
 			text(" Yes." + '\n');
-			c.setTextColor(new Color(80, 145, 180));
+			c.setTextColor(new Color(0, 204, 204));
 			text("b)");
 			c.setTextColor(Color.black);
 			text(" No." + '\n');
@@ -376,5 +502,5 @@ public class StrangerGame {
 		} while (input.equalsIgnoreCase("a"));
 		c.clear();
 		c.close(); // close the console
-	}
-}
+	} //main method
+} //StrangerGame class

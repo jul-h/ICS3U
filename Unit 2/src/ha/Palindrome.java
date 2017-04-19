1f2e0d31f8826959ab75434d0d43e176d7051e41
Julie Ha <julieha2013@gmail.com>
2017-04-19 14:29:54 -0400

    Added comments

--- Unit 2/src/ha/Palindrome.java
+++ Unit 2/src/ha/Palindrome.java
@@ -23,8 +23,7 @@ public class Palindrome {
 			// compare letter at index i with letter that's i indexes away from the last letter in word
 			if (word.charAt(i) != word.charAt(word.length()-1-i)){
 				return false;
-			}
-			
+			}	
 		}
 		return true;
 	} // isPalindrome method end
@@ -34,13 +33,15 @@ public class Palindrome {
 	// main method
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
-		String answer;
+		String answer; // user's answer to repeat program or not
 		do{
 			System.out.println("Enter a word.");
 			String word = scan.nextLine(); // get word
 			System.out.println(isPalindrome(word));
 			System.out.println("Do you want to enter another word?");
 			answer = scan.nextLine();
+			
+			// prevent program from crashing if user inputs invalid input
 			while (!(answer.equalsIgnoreCase("yes") || answer.equalsIgnoreCase("no"))){
 				System.out.println("Invalid input. Please enter 'yes' or 'no'");
 				answer = scan.nextLine();

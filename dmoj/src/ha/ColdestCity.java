da2522bd72e6b53e9653df976ef1e71af7c6961c
Julie Ha <julieha2013@gmail.com>
2017-04-29 00:59:49 -0400

    Initial commit, completed ColdestCity and QuadrantSelection

---
+++ dmoj/src/ha/ColdestCity.java
@@ -0,0 +1,25 @@
+package ha;
+
+import java.util.Scanner;
+
+public class ColdestCity {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String city = null, coldestCity = null;
+		int temp, coldestTemp = 0;
+		
+		do {
+			city = scan.next();
+			temp = scan.nextInt();
+			if (temp < coldestTemp){
+				coldestTemp = temp;
+				coldestCity = city;
+			}
+		} while (!city.equals("Waterloo"));
+		System.out.println(coldestCity);
+		
+	}
+
+}
